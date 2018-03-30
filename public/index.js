afa3ca78a1fdc9f518672b64d331fdf8041cc75f
Tiller <ctiller@proforma.com>
2018-03-30 17:09:46 -0400

    update to display all clues data upon load

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 const max = 18418;
 
 const randomizeCategory = () => {
-    console.log(Math.floor(Math.random() * max));
+    return(Math.floor(Math.random() * max));
 }
 
 angular
@@ -14,22 +14,32 @@ angular
         for(let i = 0; i < 5; i++) {
             $http({
                 method: "GET",
-                url: "http://jservice.io/api/random?"
-            }).then(response => {
-                // console.log(response.data);
-                $scope.categoryIDs.push(response.data[0].category_id);
-                return response.data[0].category_id;
-            }).then((cat_id) => {
-                console.log(cat_id);
-                $http({
-                    method: "GET",
-                    url: `http://jservice.io/api/category?&id=${cat_id}`
-                }).then(response => {
-                    console.log(response.data);
-                    $scope.categoryData.push(response.data);
-                })
+                url: `http://jservice.io/api/category?&id=${randomizeCategory()}`
+            }).then((response) => {
+                console.log(response.data);
+                $scope.categoryData.push(response.data);
             });
         }
+
+        // for(let i = 0; i < 5; i++) {
+        //     $http({
+        //         method: "GET",
+        //         url: "http://jservice.io/api/random?"
+        //     }).then(response => {
+        //         // console.log(response.data);
+        //         $scope.categoryIDs.push(response.data[0].category_id);
+        //         return response.data[0].category_id;
+        //     }).then((cat_id) => {
+        //         console.log(cat_id);
+        //         $http({
+        //             method: "GET",
+        //             url: `http://jservice.io/api/category?&id=${cat_id}`
+        //         }).then(response => {
+        //             console.log(response.data);
+        //             $scope.categoryData.push(response.data);
+        //         })
+        //     });
+        // }
 
 
     }]);
